83675a45b05739cd1762c1bef351f54516dd9234
Hariprasad <haridx.poojary@gmail.com>
2022-01-19 20:30:12 +0530

    update folder structure update docker-compose file to correct folder structure in the container and avoid error "python3: can't open file '/recipe_api/manage.py': [Errno 2] No such file or directory"

--- recipie_api/urls.py
+++ recipie_api/urls.py
@@ -1,4 +1,4 @@
-"""recipe_api URL Configuration
+"""recipie_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
