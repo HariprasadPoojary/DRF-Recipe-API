5482134eb11a4e12ec592fa4afe70f8956485a68
Hariprasad <haridx.poojary@gmail.com>
2022-01-22 15:19:17 +0530

    Add configurations for Admin GUI add custom fieldsets so that admin can change user settings in admin GUI add tests for the same

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
 
 # Register your models here.
+from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ["id"]
+    list_display = [
+        "email",
+        "name",
+    ]
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": ("email", "password"),
+            },
+        ),
+        (
+            _("Personal Info"),
+            {
+                "fields": ("name",),
+            },
+        ),
+        (
+            _("Permisssions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+        (
+            _("Important Dates"),
+            {
+                "fields": ("last_login",),
+            },
+        ),
+    )
+
+
+admin.site.register(User, UserAdmin)
---
+++ core/tests/test_admin.py
@@ -0,0 +1,40 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        # create admin user
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@domain.com",
+            password="test123",
+        )
+        # login admin user
+        self.client.force_login(self.admin_user)
+        # create normal user
+        self.user = get_user_model().objects.create_user(
+            email="testuser@domain.com",
+            password="test123",
+            name="Test User FullName",
+        )
+
+    def test_user_list(self):
+        """Test that users are listed on admin user page"""
+        # generate url rather than hard coding it
+        url = reverse("admin:core_user_changelist")
+        # get response
+        res = self.client.get(url)
+        # test
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        # generate url rather than hard coding it
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        # sample url -> /admin/core/user/1 where 1 is user id
+        res = self.client.get(url)
+        # test
+        self.assertEqual(res.status_code, 200)
